fa850832b1d6f2d7ea43963f650a7ae54734099e
Graham <s3242096@student.rmit.edu.au>
2020-01-09 16:46:58 +0800

    Update index.php
    removed old procedures
    include BTCMarkets.php
    $exchange -> BTCMarkets Class Object
    new accountbalance
    market order

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     * BTC Markets PHP REST API v3
     *
     * @author birdman
-    * @version 0.1
+    * @version 0.2
     * @link https://github.com/gbwx-net/btc_markets_dca
     * 
     * Further Reading - https://github.com/BTCMarkets/API
@@ -16,6 +16,7 @@
 
     date_default_timezone_set('Australia/Sydney');
 
+    require('BTCMarkets.php');
     require('conf.settings.php');
 ?>
 <!DOCTYPE html>
@@ -27,59 +28,26 @@
     
     echo 'Date: ' . date("D M d, Y G:i a") . '<br>';
 
-    $secret_key = $apiSecret;
-    $secret_key_encoded = base64_decode($secret_key);
-    $public_key = $apiKey;
-    $milliseconds = round(microtime(true) * 1000);
+    $exchange = new BTCMarkets($apiKey, $apiSecret);
+    $ticker = $exchange->getTicker($tradeSymbol);
+    //if(isset($_GET['debug']) && $_GET['debug'] =='true'){ echo '<pre>'.var_dump($exchange).'</pre>'; }
+    //if(isset($_GET['debug']) && $_GET['debug'] =='true'){ echo '<pre>'.var_dump($ticker).'</pre>'; }
 
-    $msg = "/account/balance\n" . $milliseconds . "\n";
-    echo "Message is: " . $msg ."<br>";
+    echo 'Last Price: '.$ticker['lastPrice'];
 
-    $encodedMsg =   hash_hmac('sha512', $msg, $secret_key_encoded, true);
-    $base64Msg = base64_encode($encodedMsg);
-    echo "Encoded Message is:  " . $base64Msg . "<br>";
+    $accountBalance = (($exchange->getBalance('AUD'))/100000000);
 
-    // Create a stream
-    $gets = array(
-        'http'=>array(
-              'method'=>"GET",
-              'header'=>      "Accept: */*\r\n" .
-                              "Accept-Charset: UTF-8\r\n" .
-                              "Content-Type: application/json\r\n" .
-                              "apikey: " . $public_key . "\r\n" .
-                              "timestamp: " . $milliseconds . "\r\n" .
-                              "User-Agent: btc markets php client\r\n" .
-                              "signature: " . $base64Msg . "\r\n"
-        )
-    );
+    if($accountBalance > $tradeAmount)
+    {
+        $orderMarketID = 'BTC-AUD';
+        $orderPrice = $ticker['bestAsk'];
+        $orderAmount = number_format(($tradeAmount / $orderPrice), 8);
+        $orderAmountString = (string)$orderAmount;
+        $orderType = 'Market';
+        $orderSide = 'Bid';
+    }
 
-    $posts = array(
-        'http'=>array(
-              'method'=>"POST",
-              'header'=>      "Accept: */*\r\n" .
-                              "Accept-Charset: UTF-8\r\n" .
-                              "Content-Type: application/json\r\n" .
-                              "apikey: " . $public_key . "\r\n" .
-                              "timestamp: " . $milliseconds . "\r\n" .
-                              "User-Agent: btc markets php client\r\n" .
-                              "signature: " . $base64Msg . "\r\n"
-        )
-    );
-
-    $context = stream_context_create($gets);
-    if(isset($_GET['debug']) && $_GET['debug'] == 'true'){ echo '<pre>'.var_dump($gets).'</pre>'; }
-    
-    // Open the file using the HTTP headers set above
-    $fileGet = file_get_contents('https://api.btcmarkets.net/account/balance', false, $context);
-    $jsonAccount = json_decode($fileGet, true);
-    if(isset($_GET['debug']) && $_GET['debug'] == 'true'){ echo '<pre>'.var_dump($jsonAccount).'</pre>'; }
-
-    // Get BTC-AUD Price
-    $fileGet = file_get_contents('https://api.btcmarkets.net/v3/markets/BTC-AUD/ticker', false, $context);
-    $jsonTicker = json_decode($fileGet, true);
-    if(isset($_GET['debug']) && $_GET['debug'] =='true'){ echo '<pre>'.var_dump($jsonTicker).'</pre>'; }
-
-    echo '$'.(($jsonAccount[0]['balance'])/100000000).' '.$jsonAccount[0]['currency'];
+    $order = $exchange->createMarketOrder($orderMarketID, $orderPrice, $orderAmountString, $orderType, $orderSide);
 ?>
     </body>
 </html>
